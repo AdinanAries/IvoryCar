09f00fd72aa2b7ca926cfd9edbb16e0a597410d7
Mohammed Adinan <adinanaries@outlook.com>
2020-10-12 22:06:23 -0400

    added basic homepage components with initial styles

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server started on " + PORT);
+});
