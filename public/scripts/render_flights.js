d2715c5ad98cbb1452e986344045075b4d70aa1f
Mohammed Adinan <adinanaries@outlook.com>
2020-12-25 15:17:46 -0500

    ...

--- public/scripts/render_flights.js
+++ public/scripts/render_flights.js
@@ -310,18 +310,18 @@ function render_flights(){
                         <div class="bubble_popup arrow_on_right_side">
                         <p style="font-size: 12px; text-align: justify; margin: 10px; opacity: 0.6;">
                             Flight ticket prices fluctuate constantly – demand, availability,
-                            route, date and even search time all play a role in determining the price offered.
-                            this rating is based on historical flight prices for your chosen route(s) to help
+                            route, date and even search time all play a role in determining the price offered.<br/><br/>
+                            This rating is based on historical flight prices for your chosen route(s) to help
                             you understand if you're getting a good deal
                          </p>
-                         <p style="font-size: 12px; font-weight: bolder; 10px; opacity: 0.8; color: #e25a00; margin: 0 10px; margin-top:15px;  margin-bottom: 5px;">Price Stats</p>
+                         <p style="font-size: 12px; font-weight: bolder; 10px; opacity: 0.8; color: #e25a00; margin: 0 10px; margin-top:15px;  margin-bottom: 5px;">Price Stats For Each Adult</p>
 
                          <p style="font-size: 13px; margin: 3px 10px;">MINIMUM: 
-                         <span style="font-size: 14px; color: #e25a00;">${current_currency.currency} ${price_metrics_min}</span></p>
+                         <span style="font-size: 14px; color: #e25a00;">${current_currency.sign} ${price_metrics_min.toFixed(2)}</span></p>
                          <p style="font-size: 13px; margin: 3px 10px;">MEDIUM: 
-                         <span style="font-size: 14px; color: #e25a00;">${current_currency.currency} ${price_metrics_medium}</span></p>
+                         <span style="font-size: 14px; color: #e25a00;">${current_currency.sign} ${price_metrics_medium.toFixed(2)}</span></p>
                          <p style="font-size: 13px; margin: 3px 10px;">MAXIMUM: 
-                         <span style="font-size: 14px; color: #e25a00;">${current_currency.currency} ${price_metrics_max}</span></p>
+                         <span style="font-size: 14px; color: #e25a00;">${current_currency.sign} ${price_metrics_max.toFixed(2)}</span></p>
 
                         </div>
                         </div>
