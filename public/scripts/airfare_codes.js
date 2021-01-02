454ab888622a54752a189ea2db7a8cf3d0500b98
Mohammed Adinan <adinanaries@outlook.com>
2021-01-02 10:46:59 -0500

    added airfare codes

---
+++ public/scripts/airfare_codes.js
@@ -0,0 +1,26 @@
+var airfare_codes = [
+    {code: "A", fare: "First Class Discounted"},
+    {code: "B", fare: "Economy/Coach"}, // – Usually an upgradable fare to Business
+    {code: "C", fare: "Business Class"},
+    {code: "D", fare: "Business Class Discounted"},
+    {code: "E", fare: "Shuttle Service"}, // (no reservation allowed) or Economy/Coach Discounted
+    {code: "F", fare: "First Class"},
+    {code: "G", fare: "Conditional Reservation"},
+    {code: "H", fare: "Economy/Coach Discounted"}, // – Usually an upgradable fare to Business
+    {code: "J", fare: "Business Class Premium"},
+    {code: "K", fare: "Economy/Coach Discounted"},
+    {code: "L", fare: "Economy/Coach Discounted"},
+    {code: "M", fare: "Economy/Coach Discounted"}, // – Usually an upgradable fare to Business
+    {code: "N", fare: "Economy/Coach Discounted"},
+    {code: "P", fare: "First Class Premium"},
+    {code: "Q", fare: "Economy/Coach Discounted"},
+    {code: "R", fare: "First Class Suite"},// or Supersonic (discontinued)
+    {code: "S", fare: "Economy/Coach"},
+    {code: "T", fare: "Economy/Coach Discounted"},
+    {code: "U", fare: "Shuttle Service"}, // (no reservation needed/seat guaranteed)
+    {code: "V", fare: "Economy/Coach Discounted"},
+    {code: "W", fare: "Economy/Coach Premium"},
+    {code: "X", fare: "Economy/Coach Discounted"},
+    {code: "Y", fare: "Economy/Coach"},
+    {code: "Z", fare: "Business Class Discounted"},
+]
