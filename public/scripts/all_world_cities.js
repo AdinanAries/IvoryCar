dbb76db3f59688f49e63b33e124be49e5460d6a6
Mohammed Adinan <adinanaries@outlook.com>
2021-02-22 19:52:17 -0500

    econding string params for function to pick item from autocomplete - this encoding ensures that single qoutes characters are replaced with special characters in order to not disrupt string open and closing single qoutes

--- public/scripts/all_world_cities.js
+++ public/scripts/all_world_cities.js
@@ -4400,7 +4400,7 @@ function return_dom_for_all_wordl_cities(cities_arr){
     let dom_arr = cities_arr.map(each =>{
 
         return `
-        <li onclick="pick_selected_city_from_all_world_cities_autocomplete('${each.name}','${each.country}');" style="cursor: pointer; padding: 5px 10px; overflow: initial !important;" >
+        <li onclick="pick_selected_city_from_all_world_cities_autocomplete('${each.name.replaceAll('\'', '#$#$%$#098')}','${each.country.replaceAll('\'', '#$#$%$#098')}');" style="cursor: pointer; padding: 5px 10px; overflow: initial !important;" >
             <p style="font-size: 14px; color:rgb(8, 77, 122); overflow: initial !important;">
             <i style="margin-right: 5px; color:rgb(235, 86, 0);" class="fa fa-map-marker" aria-hidden="true"></i>
             ${each.name}, ${each.country}</p>
@@ -4429,9 +4429,12 @@ search_cheap_hotels_by_location_text_field.addEventListener("input", evnt =>{
 
 function pick_selected_city_from_all_world_cities_autocomplete(city, country){
 
-    search_cheap_hotels_by_location_text_field.value = `${city}, ${country}`;
-    search_cheap_hotels_post_data.city = city;
-    search_cheap_hotels_post_data.country = country;
+    let a_city = city.replaceAll('#$#$%$#098', "'");
+    let a_country = country.replaceAll('#$#$%$#098', "'");
+
+    search_cheap_hotels_by_location_text_field.value = `${a_city}, ${a_country}`;
+    search_cheap_hotels_post_data.city = a_city;
+    search_cheap_hotels_post_data.country = a_country;
 
     $("#main_book_cheap_book_direct_location_input_autocomplete_section").slideUp("fast");
 }
