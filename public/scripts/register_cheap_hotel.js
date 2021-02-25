d78e1c7cb91af26b5fba12f4051f2547fe16023f
Mohammed Adinan <adinanaries@outlook.com>
2021-02-25 12:06:44 -0500

    now displaying hotels photo before upload on register cheap hotel form

--- public/scripts/register_cheap_hotel.js
+++ public/scripts/register_cheap_hotel.js
@@ -3,3 +3,14 @@ var book_cheap_hotel_register_new_hotel_button = document.getElementById("book_c
 book_cheap_hotel_register_new_hotel_button.addEventListener("click", evnt => {
     alert("here");
 });
+
+
+function cheap_hotel_preview_image(event, elem) {
+    var reader = new FileReader();
+    reader.onload = function()
+    {
+        var output = document.getElementById(elem);
+        output.style.backgroundImage = `url('${reader.result}')`;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
